8303c4432c552b81ca5a744727d07d4ad12f9fe3
Chandrasekhar Kappagantula <85963035+closinator@users.noreply.github.com>
2024-04-03 09:33:21 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const astro = document.getElementsByClassName(".astro");
+
+function astrophotography() {
+  window.location.href = "astrophotography.html";
+}
+
+const bin = document.getElementsByClassName(".bin");
+
+function astrobin() {
+  window.location.href = "https://www.astrobin.com/users/Chandrasekhar_Kapppagantula/?_ga=2.61582636.1807423039.1705243054-183872626.1696907685";
+}
