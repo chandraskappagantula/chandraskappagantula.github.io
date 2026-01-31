e0d93003af43d02ccf3f2ff260380e3c966a9b0e
Chandrasekhar Kappagantula <85963035+chandraskappagantula@users.noreply.github.com>
2026-01-31 21:08:21 +0000

    Actually build the npm

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
 import { Intro } from "./components/Intro"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <main>
